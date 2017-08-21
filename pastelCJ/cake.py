0d06a5e42063960240355bea5658646b5333a835
waffer95 <delkys.yo@gmail.com>
2017-08-20 23:12:22 -0400

    Now it works with the Large dataset! Plus Ultragit checkout master :)

--- pastelCJ/cake.py
+++ pastelCJ/cake.py
@@ -21,18 +21,18 @@ def is_empty_row(row):
 
 def dist_cake(cake, R, C):
     #while has_empty_cells(cake, R, C):
-    #print ("Entrando en el ciclo 'while has_empty_cells(cake, R, C)'")
+    #print ("entering in loop 'while has_empty_cells(cake, R, C)'")
     # searching for letters in the cake
     for i in range(R):
         for j in range(C):
             if cake[i][j] >= 'A' or cake[i][j] <= 'Z':
-                # sustituing from the letter to the right
+                # substituting from the letter to the right
                 for jj in range(j + 1, C):
                     if cake[i][jj] is '?':
                         cake[i][jj] = cake[i][j]
                     elif cake[i][jj] is not cake[i][j] and cake[i][jj] is not '?':
                         break
-                # sustituing from the letter to the left
+                # substituting from the letter to the left
                 for jj in range(j, -1, -1):
                     if cake[i][jj] is '?':
                         cake[i][jj] = cake[i][j]
@@ -47,35 +47,29 @@ def dist_cake(cake, R, C):
                 last_empty_row = i
                 break
 
-        # searching for a non-empty row after the last_empty_row
-        last_non_empty_row = None
-        for i in range(last_empty_row, R):
-            if not(is_empty_row(cake[i])):
-                last_non_empty_row = i
-                break
+        if last_empty_row is not None:
+            # searching for a non-empty row after the last_empty_row
+            last_non_empty_row = None
+            for i in range(last_empty_row, R):
+                if not(is_empty_row(cake[i])):
+                    last_non_empty_row = i
+                    break
 
-        # debug printing
-        print ("X" * 5,"CAKE", "X" * 5)
-        for row in cake:
-            print (row)
+            # if we cannot find something forward, we find it backwards
+            if last_non_empty_row is None:
+                for i in range(last_empty_row, -1, -1):
+                    if not(is_empty_row(cake[i])):
+                        last_non_empty_row = i
+                        break
 
         if last_empty_row is not None:
-            print ("there is a empty row at", last_empty_row)
-            if last_empty_row is R - 1:
-                if is_empty_row(cake[last_empty_row - 1]):
-                    for jj in range(C):
-                        cake[last_empty_row][jj] = cake[last_empty_row - 2][jj]
-                else:
-                    for jj in range(C):
-                        cake[last_empty_row][jj] = cake[last_empty_row - 1][jj]
-            else:
-                for jj in range(C):
-                    cake[last_empty_row][jj] = cake[last_empty_row + 1][jj]
-        else:
-            print ("there is not a empty row at", last_empty_row)
+            for jj in range(C):
+                cake[last_empty_row] = cake[last_non_empty_row]     
         
     return cake
 
+
+##################### main #####################
 
 
 T = int(input())
