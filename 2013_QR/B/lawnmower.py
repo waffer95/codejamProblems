9ecd92bb89ed232af7c35da427b23ce81ec057d4
Delkys Welffer <delkys.yo@gmail.com>
2018-03-17 14:56:49 -0400

    Fixed the 2013_QR_A source code problem, now it pass the test on codejam's platform :)

--- 2013_QR/B/lawnmower.py
+++ 2013_QR/B/lawnmower.py
@@ -3,34 +3,43 @@ Google Codejam 2013 Qualification Round; Problem B; The Lawnmower
 by DelkysWelffer
 """
 
-def solve(lawn, N, M):
+def print_sq(sq):
+	for row in sq:
+		print (row)
+	print ()
+
+def solve(pattern, N, M):
 	if N == 1 or M == 1:
 		return "YES"
 
-	internals = set()
-	for i in range(1, N - 1):
-		for j in range(1, M - 1):
-			internals.add(lawn[i][j])
-			
-	externals = set()
-	for row in lawn:
-		externals.add(row[0])
-		externals.add(row[-1])
-	for chunk in lawn[0]:
-		externals.add(chunk)
-	for chunk in lawn[-1]:
-		externals.add(chunk)
-		
-	for internal in internals:
-		for external in externals:
-			if internal < external:
-				return "NO"
+	lawn = [ [100 for _ in range(M)] for _ in range(N) ]
 
-	return "YES"
+	for i in range(N):
+		maxInRow = 0
+		for j in range(M):
+			if pattern[i][j] > maxInRow:
+				maxInRow = pattern[i][j]
+		for j in range(M):
+			if maxInRow < lawn[i][j]:
+				lawn[i][j] = maxInRow
+	
+	for j in range(M):
+		maxInCol = 0
+		for i in range(N):
+			if pattern[i][j] > maxInCol:
+				maxInCol = pattern[i][j]
+		for i in range(N):
+			if maxInCol < lawn[i][j]:
+				lawn[i][j] = maxInCol
+
+	if lawn == pattern:
+		return "YES"
+
+	return "NO"
 
 
 if __name__ == '__main__':
 	for T in range(1, int(input()) + 1):
 		N, M = ( int(n) for n in input().split(' ') )
-		lawn = [ [ int(n) for n in input().split(' ') ] for _ in range(N) ]
-		print ("Case {}: {}".format(T, solve(lawn, N, M)))
+		pattern = [ [ int(n) for n in input().split(' ') ] for _ in range(N) ]
+		print ("Case #{}: {}".format(T, solve(pattern, N, M)))
